55f4bc3f20b3bf8bd2959f34bce5cf6d796970a8
Alessio-Gasbarro <gasbarro.developer@gmail.com>
2025-04-16 17:36:15 +0200

    [UPD] Fixed dolciblog

--- app.js
+++ app.js
@@ -22,36 +22,36 @@ const dolciblog = [
     {
         title: "Ciambellone",
         content: "Torta rotonda, come una ciambella!",
-        img: "<img src=./images/ciambellone.jpeg>",
-        tags: ["Tag1", "Tag2", "Tag3"],
+        img: "./images/ciambellone.jpeg",
+        tags: ["Zucchero", "Burro", "Uova"],
     },
     {
         title: "Cracker Barbabietola",
         content: "Non so cosa sia questo orrore.",
-        img: "<img src=./images/cracker_barbabietola.jpeg>",
-        tags: ["Tag1", "Tag2", "Tag3"],
+        img: "./images/cracker_barbabietola.jpeg",
+        tags: ["Farina", "Acqua", "Barbabietola"],
     },
     {
         title: "Pane Fritto dolce",
         content: "Praticamente le pettole pugliesi!",
-        img: "<img src=./images/pane_fritto_dolce.jpeg>",
-        tags: ["Tag1", "Tag2", "Tag3"],
+        img: "./images/pane_fritto_dolce.jpeg",
+        tags: ["Farina", "Zucchero", "Olio"],
     },
     {
         title: "Pasta Barbabietola",
         content: "Stessa cosa dei cracker ma bolliti! (Scherzo)",
-        img: "<img src=./images/pasta_barbabietola.jpeg>",
-        tags: ["Tag1", "Tag2", "Tag3"],
+        img: "./images/pasta_barbabietola.jpeg",
+        tags: ["Farina", "Barbabietola", "Pazienza"],
     },
     {
         title: "Torta Paesana",
         content: "Torta al gusto di torta, ma più buona!",
-        img: "<img src=./images/torta_paesana.jpeg>",
-        tags: ["Tag1", "Tag2", "Tag3"],
+        img: "./images/torta_paesana.jpeg",
+        tags: ["Torta", "Torta", "Torta"],
     },
 ];
 
 //Array "dolciblog" postato in Post>3000/catalogo
-app.post("/catalogo", (req, res) => {
-    res.send(dolciblog);
+app.use("/catalogo", express.static('public'), (req, res) => {
+    res.json(dolciblog);
 });
